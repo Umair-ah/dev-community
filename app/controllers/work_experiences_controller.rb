a7c277ccd0ea2cd0b303ff295c11c5796bf53b2d
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-11-09 21:19:59 +0530

    turbo method for delete and also fixed small bugs

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -52,7 +52,17 @@ class WorkExperiencesController < ApplicationController
 
   end
 
-  def destroy; end
+  def destroy
+    respond_to do |format|
+      @work_experience.destroy
+      format.turbo_stream{
+        render turbo_stream:
+        turbo_stream.remove(
+          "work_experience_item_#{@work_experience.id}",
+        )
+      }
+    end
+  end
 
 
   private
